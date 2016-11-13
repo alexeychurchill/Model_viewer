11a118f022d42fdcac50d62f66dffe53f63ce871
Alexey <samolet15@mail.ru>
2016-11-13 19:06:43 +0200

    Updated Renderer: back-face culling moved into separated method

--- app/src/main/java/io/github/alexeychurchill/modelviewer/graphics/Renderer.java
+++ app/src/main/java/io/github/alexeychurchill/modelviewer/graphics/Renderer.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.util.Log;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -78,18 +79,18 @@ public class Renderer {
     }
 
     public List<Polygon2d> projectModel(Model3d model3d) {
-        List<Polygon2d> polygons2d = new LinkedList<>();
         // FIXME: 13.11.2016 Implement callback, which can tell how many total faces and visible faces
-        lastTotalFaces = model3d.getPolygons().size();
-        for (Polygon3d polygon3d : model3d.getPolygons()) {
-            if (backfaceCullingEnabled) { // TODO: 13.11.2016 Implement method, which will perform back face culling
-                if (polygonVisible(polygon3d)) {
-                    polygons2d.add(projectPolygon(polygon3d));
-                }
-            } else {
-                polygons2d.add(projectPolygon(polygon3d));
-            }
+        List<Polygon2d> polygons2d = new LinkedList<>();
+        List<Polygon3d> modelPolygons = model3d.getPolygons();
+        lastTotalFaces = modelPolygons.size();
+
+        if (backfaceCullingEnabled) {
+            int removed = performBackfaceCulling(modelPolygons);
         }
+        for (Polygon3d polygon3d : modelPolygons) {
+            polygons2d.add(projectPolygon(polygon3d));
+        }
+
         lastVisibleFaces = polygons2d.size();
         return polygons2d;
     }
@@ -110,7 +111,6 @@ public class Renderer {
         this.backfaceCullingEnabled = backfaceCullingEnabled;
     }
 
-    // TODO: 13.11.2016 Render bitmap here
     public Bitmap renderToBitmap(Model3d model, int width, int height, int backgroundColor) {
         Bitmap renderedBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Canvas renderCanvas = new Canvas(renderedBitmap);
@@ -159,5 +159,20 @@ public class Renderer {
         path.close();
 
         return path;
+    }
+
+    /*
+    * This performs backface culling
+    * */
+    private int performBackfaceCulling(List<Polygon3d> polygons) {
+        int removedCount = 0;
+        for (int polygonNumber = 0; polygonNumber < polygons.size(); polygonNumber++) {
+            if (!polygonVisible(polygons.get(polygonNumber))) {
+                polygons.remove(polygonNumber);
+                removedCount++;
+                polygonNumber--;
+            }
+        }
+        return removedCount;
     }
 }
